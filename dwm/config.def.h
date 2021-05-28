6c82e2f4cee888c263b135ec55b2500903fe7fc1
Vlad Tutunea <vladovidiu.tutunea@gmail.com>
2021-05-28 09:06:16 +0100

    add emacsclient keybind

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -104,6 +104,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_e,      spawn,          SHCMD("emacsclient -cna 'emacs'") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
