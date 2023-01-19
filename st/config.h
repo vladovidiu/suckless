ef94343a3e9efda7a599c50c34c74c8b9d53c112
Vlad Tutunea <vladovidiu.tutunea@gmail.com>
2023-01-19 21:38:14 +0000

    finally fix glyphs alignment

--- st/config.h
+++ st/config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font =
-    "PragmataPro Mono Liga:pixelsize=20:antialias=true:autohint=true";
+    "PragmataPro Mono Liga:pixelsize=22:antialias=true:autohint=true";
 static char *font2[] = {"JoyPixels:pixelsize=10:antialias=true:autohint=true"};
 static int borderpx = 0;
 
